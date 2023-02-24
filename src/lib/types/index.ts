14c3b18ebc0df02a155256bd2bd35a14c3281fec
Harlen Alvarez <harlenalvarez@gmail.com>
2023-02-24 16:06:41 -0500

    feat: add aligment and baseline type

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,3 +1,6 @@
 export * from './Shapes';
 export type Optional<T, K extends keyof T> = Partial<T> & Omit<T, K>;
 export type Need<T, K extends keyof T> = Partial<T> & Pick<T, K>;
+
+export type TextAlignment = 'start' | 'end' | 'left' | 'center' | 'right';
+export type TextBaseline = 'top' | 'hanging' | 'middle' | 'alphabetic' | 'ideographic' | 'bottom';
